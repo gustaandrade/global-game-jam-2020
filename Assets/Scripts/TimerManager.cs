e74496a5ed8070269ac87ec9217ccc72c5d4e56b
Gustavo Andrade <gustavo.gustaandrade@gmail.com>
2020-02-01 17:06:19 -0300

    Merge of "master" and "origin/master"

--- Assets/Scripts/TimerManager.cs
+++ Assets/Scripts/TimerManager.cs
@@ -1,14 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UI.Extensions;
 
 public class TimerManager : MonoBehaviour
 {
     public static TimerManager Instance;
 
+    public float GlobalTimer;
+    public RadialSlider GlobalTimerSlider;
+    public Image GlobalTimerImage;
+
+    public float CallEventTimer;
+    public float CallEventLimitTimer;
+
     private void Start()
     {
         if (Instance == null)
             Instance = this;
+    }
+
+    private void FixedUpdate()
+    {
+        GlobalTimer += Time.fixedDeltaTime;
+        CallEventTimer += Time.fixedDeltaTime;
+        GlobalTimerSlider.Value = GlobalTimer;
+        GlobalTimerImage.fillMethod = Image.FillMethod.Horizontal;
+        //GlobalTimerImage.fillAmount = GlobalTimer / 10;
+
+        if (CallEventTimer >= CallEventLimitTimer)
+        {
+            EventManager.instance.StartEventInRndGrave();
+            CallEventTimer = 0f;
+        }
     }
 }
