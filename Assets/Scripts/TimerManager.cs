9e8337918edc734b9313e338548ec2ed75c1d2e0
Gustavo Andrade <gustavo.gustaandrade@gmail.com>
2020-02-01 14:27:56 -0300

    Adds DOTween and UIExtensions assets to the project
    It will be worth it. Trust me.

---
+++ Assets/Scripts/TimerManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerManager : MonoBehaviour
+{
+    public static TimerManager Instance;
+
+    private void Start()
+    {
+        if (Instance == null)
+            Instance = this;
+    }
+}
