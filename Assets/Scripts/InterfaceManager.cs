7b7bf2fabe289f00aada8d00e6b745202e608afa
Gustavo Andrade <gustavo.gustaandrade@gmail.com>
2020-02-02 15:30:43 -0300

    More quality of live changes for v3
    Seriously...

---
+++ Assets/Scripts/InterfaceManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterfaceManager : MonoBehaviour
+{
+    public AudioClip Click;
+    private AudioSource _source;
+
+    private void Start()
+    {
+        _source = GetComponent<AudioSource>();
+    }
+
+    private void OnMouseDown()
+    {
+        _source.PlayOneShot(Click);
+    }
+}
