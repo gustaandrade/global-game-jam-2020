15f58e02977eb3b22aa29bf841621f7436e8f916
Gustavo Andrade <gustavo.gustaandrade@gmail.com>
2020-02-01 21:08:21 -0300

    Makes V1 of MainMenu routine

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class SceneManager : MonoBehaviour
+{
+    public static SceneManager Instance;
+
+    [Space(10), Header("Objects")]
+    public RectTransform MovingObjects;
+
+    [Space(10), Header("Values")] 
+    public float MovingMenuEndPosX = 1280f;
+    public float MovingMenuEndPosYUp = -800f;
+    public float MovingMenuEndPosYDown = 800f;
+    public float MovingMenuDurationX = 4f;
+    public float MovingMenuDurationY = 2f;
+
+    private void Start()
+    {
+        if (Instance == null)
+            Instance = this;
+    }
+
+    public void OnPlay()
+    {
+        MovingObjects.DOLocalMoveX(MovingMenuEndPosX, MovingMenuDurationX).SetEase(Ease.Linear);
+    }
+
+    public void OnAbout()
+    {
+        MovingObjects.DOLocalMoveY(MovingMenuEndPosYUp, MovingMenuDurationY).SetEase(Ease.Linear);
+    }
+
+    public void OnBackAbout()
+    {
+        MovingObjects.DOLocalMoveY(0f, MovingMenuDurationY).SetEase(Ease.Linear);
+    }
+
+    public void OnExit()
+    {
+        MovingObjects.DOLocalMoveY(MovingMenuEndPosYDown, MovingMenuDurationY).SetEase(Ease.Linear);
+    }
+
+    public void OnBackExit()
+    {
+        MovingObjects.DOLocalMoveY(0f, MovingMenuDurationY).SetEase(Ease.Linear);
+    }
+
+    public void OnClose()
+    {
+        Application.Quit();
+    }
+}
