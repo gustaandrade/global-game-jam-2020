34b7a4c44d5a0314a0624c73d99dd9c66538b5f9
Gustavo Andrade <gustavo.gustaandrade@gmail.com>
2020-02-02 02:39:25 -0300

    Somehow makes V1 of changing day/night
    Almost there, I think

--- Assets/Scripts/TimerManager.cs
+++ Assets/Scripts/TimerManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UI.Extensions;
@@ -8,13 +9,23 @@ public class TimerManager : MonoBehaviour
 {
     public static TimerManager Instance;
 
-    [SerializeField, ReadOnly] private float _globalTimer;
-    public float GlobalTimerLimit;
+    [Space(10), Header("Objects")]
     public RadialSlider GlobalTimerSlider;
     public Image GlobalTimerImage;
+    public GameObject Transition;
+    public TextMeshProUGUI WaveText;
+    public TextMeshProUGUI TimeOfDayText;
 
+    [Space(10), Header("Values")]
+    [SerializeField, ReadOnly] private float _globalTimer;
+    public float GlobalTimerLimit;
+    
     [SerializeField, ReadOnly] private float _callEventTimer;
     public float CallEventLimitTimer;
+
+    private bool _rolling = true;
+    private int _waveCount = 1;
+    private bool _isNightTime = true;
 
     private void Start()
     {
@@ -24,10 +35,16 @@ public class TimerManager : MonoBehaviour
 
     private void FixedUpdate()
     {
+        TimeOfDayText.text = _isNightTime ? "Night" : "Day";
+        WaveText.text = $"Wave {_waveCount}";
+
+        if (!_rolling) return;
+
         _globalTimer += Time.fixedDeltaTime;
         _callEventTimer += Time.fixedDeltaTime;
 
         GlobalTimerSlider.Value = _globalTimer / GlobalTimerLimit;
+
         // don't ask why this stupid line is here, just don't...
         GlobalTimerImage.fillMethod = Image.FillMethod.Horizontal;
 
@@ -40,7 +57,23 @@ public class TimerManager : MonoBehaviour
         if (_globalTimer >= GlobalTimerLimit)
         {
             _globalTimer = 0f;
-            Debug.Log("cycle time");
+            _rolling = false;
+            Transition.SetActive(true);
+
+            _isNightTime = !_isNightTime;
+            
+            StartCoroutine(FreeRolling());
         }
+    }
+
+    private IEnumerator FreeRolling()
+    {
+        yield return new WaitForSeconds(10f);
+
+        _isNightTime = !_isNightTime;
+        _waveCount++;
+
+        Transition.SetActive(false);
+        _rolling = true;
     }
 }
