54948dee9b5168b793a36e6fd3837a783554dd2f
Gustavo Andrade <gustavo.gustaandrade@gmail.com>
2020-02-02 11:38:33 -0300

    Pause Routine working, ,minus sounds
    Doing it now.

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
-public class SoundManager : MonoBehaviour
+public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioMixer BGM;
+    public AudioMixer SFX;
+
+    private const float SOUND_OFF = -80f;
+    private const float SOUND_ON = 0f;
+
+    private bool _isBGMOn = true;
+    private bool _isSFXOn = true;
+
+    private void Start()
     {
-        
+        BGM.SetFloat("Volume", SOUND_ON);
+        SFX.SetFloat("Volume", SOUND_ON);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeBGM()
     {
-        
+        BGM.SetFloat("Volume", _isBGMOn ? SOUND_OFF : SOUND_ON);
+        _isBGMOn = !_isBGMOn;
+    }
+
+    public void ChangeSFX()
+    {
+        SFX.SetFloat("Volume", _isSFXOn ? SOUND_OFF : SOUND_ON);
+        _isSFXOn = !_isSFXOn;
     }
 }
---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject PauseFrame;
+    public Animator Fade;
+
+    public void PauseGame()
+    {
+        PauseFrame.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        PauseFrame.SetActive(false);
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1f;
+        StartCoroutine(FadeOutRoutine());
+    }
+
+    private IEnumerator FadeOutRoutine()
+    {
+        Fade.SetTrigger("DoFadeIn");
+        yield return new WaitForSeconds(2f);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+}
